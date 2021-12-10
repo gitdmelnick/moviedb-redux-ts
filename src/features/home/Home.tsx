efa492f23e3b6d660e0cf79f784f9976bd18f13e
Dmitriy Melnichenko <melnichenko.dmytro@gmail.com>
2021-12-10 16:11:26 +0200

    Add Home component

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,10 +1,30 @@
-
+import s from "./Home.module.css";
+import { useGetMovieQuery } from "../movies/moviesSlice";
+import { apiConstants } from "../../app/constants";
+import { useEffect } from "react";
 
 const Home = () => {
-  return (
-    <>
+  const { data, isSuccess } = useGetMovieQuery(624860);
+  const style = {
+    backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.4),  rgba(0, 0, 0, 0.4)), 
+    url(${apiConstants.BACKDROP_URL}${data?.backdrop_path})`,
+  };
 
-    </>
+  useEffect(() => {}, [isSuccess]);
+
+  return (
+    <div className={s["home-container"]}>
+      <div className={s["home-backdrop"]} style={{ ...style }}>
+        <div className={s["home-text"]}>
+          <h1 className="heading-2" style={{ color: "#fff" }}>
+            Welcome
+          </h1>
+          <h2 className="heading-3" style={{ color: "#fff" }}>
+            Millions of movies, TV shows and people to discover. Explore now.
+          </h2>
+        </div>
+      </div>
+    </div>
   );
 };
 
